1be00f4387b0df1c9caf018724f6f43b06399fde
vasanth <vasanthbillov@gmail.com>
2022-07-03 13:42:51 +0530

    files addedd

--- model.py
+++ model.py
@@ -32,8 +32,8 @@ class SentimentRecommenderModel:
         self.data = pd.read_csv("data/sample30.csv")
         self.cleaned_data = pickle.load(open(
             SentimentRecommenderModel.ROOT_PATH + SentimentRecommenderModel.CLEANED_DATA, 'rb'))
-        self.lemmatizer = WordNetLemmatizer()
-        self.stop_words = set(stopwords.words('english'))
+        # self.lemmatizer = WordNetLemmatizer()
+        # self.stop_words = set(stopwords.words('english'))
 
     def getRecommendationByUser(self, user):
         return list(self.user_final_rating.loc[user].sort_values(ascending=False)[0:20].index)
@@ -65,47 +65,47 @@ class SentimentRecommenderModel:
 
     """function to classify the sentiment to 1/0 - positive or negative - using the trained ML model"""
 
-    def classify_sentiment(self, review_text):
-        review_text = self.preprocess_text(review_text)
-        X = self.vectorizer.transform([review_text])
-        y_pred = self.model.predict(X)
-        return y_pred
+    # def classify_sentiment(self, review_text):
+    #     review_text = self.preprocess_text(review_text)
+    #     X = self.vectorizer.transform([review_text])
+    #     y_pred = self.model.predict(X)
+    #     return y_pred
 
-    def preprocess_text(self, text):
-        # cleaning the review text 
-        text = text.lower().strip()
-        text = re.sub("\[\s*\w*\s*\]", "", text)
-        dictionary = "abc".maketrans('', '', string.punctuation)
-        text = text.translate(dictionary)
-        text = re.sub("\S*\d\S*", "", text)
-        # remove stop-words and convert it to lemma
-        text = self.lemma_text(text)
-        return text
+    # def preprocess_text(self, text):
+    #     # cleaning the review text 
+    #     text = text.lower().strip()
+    #     text = re.sub("\[\s*\w*\s*\]", "", text)
+    #     dictionary = "abc".maketrans('', '', string.punctuation)
+    #     text = text.translate(dictionary)
+    #     text = re.sub("\S*\d\S*", "", text)
+    #     # remove stop-words and convert it to lemma
+    #     text = self.lemma_text(text)
+    #     return text
 
-    def get_wordnet_pos(self, tag):
-        if tag.startswith('J'):
-            return wordnet.ADJ
-        elif tag.startswith('V'):
-            return wordnet.VERB
-        elif tag.startswith('N'):
-            return wordnet.NOUN
-        elif tag.startswith('R'):
-            return wordnet.ADV
-        else:
-            return wordnet.NOUN
+    # def get_wordnet_pos(self, tag):
+    #     if tag.startswith('J'):
+    #         return wordnet.ADJ
+    #     elif tag.startswith('V'):
+    #         return wordnet.VERB
+    #     elif tag.startswith('N'):
+    #         return wordnet.NOUN
+    #     elif tag.startswith('R'):
+    #         return wordnet.ADV
+    #     else:
+    #         return wordnet.NOUN
 
-    def remove_stopword(self, text):
-        words = [word for word in text.split() if word.isalpha()
-                 and word not in self.stop_words]
-        return " ".join(words)
+    # def remove_stopword(self, text):
+    #     words = [word for word in text.split() if word.isalpha()
+    #              and word not in self.stop_words]
+    #     return " ".join(words)
 
-    def lemma_text(self, text):
-        word_pos_tags = nltk.pos_tag(word_tokenize(
-            self.remove_stopword(text)))
+    # def lemma_text(self, text):
+    #     word_pos_tags = nltk.pos_tag(word_tokenize(
+    #         self.remove_stopword(text)))
         
-        words = [self.lemmatizer.lemmatize(tag[0], self.get_wordnet_pos(
-            tag[1])) for idx, tag in enumerate(word_pos_tags)]
-        return " ".join(words)
+    #     words = [self.lemmatizer.lemmatize(tag[0], self.get_wordnet_pos(
+    #         tag[1])) for idx, tag in enumerate(word_pos_tags)]
+    #     return " ".join(words)
 
 
 if __name__ == '__main__':
